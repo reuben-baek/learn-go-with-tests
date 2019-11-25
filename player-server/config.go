b538612427e37d2505050cc3268c7a6e6ec38d36
reuben.baek <reuben.baek@kakaocorp.com>
2019-11-25 09:25:18 +0900

    inject FileSystemPlayerStore to player store.

--- player-server/config.go
+++ player-server/config.go
@@ -3,7 +3,19 @@ package main
 import (
 	"github.com/reuben-baek/learn-go-with-tests/player-server/endpoint"
 	"github.com/reuben-baek/learn-go-with-tests/player-server/infrastructure"
+	"log"
+	"os"
 )
 
-var inMemoryPlayerStore = infrastructure.NewInMemoryPlayerStore()
-var playerServer = endpoint.NewPlayerServer(inMemoryPlayerStore)
+const dbFileName = "game.db.json"
+
+var db = func() *os.File {
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+	return db
+}()
+
+var fileSystemPlayerStore = infrastructure.NewFileSystemPlayerStore(db)
+var playerServer = endpoint.NewPlayerServer(fileSystemPlayerStore)
